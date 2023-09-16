8a783a58afba3733dcd8d0be1bec4e5e8f6486a1
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2023-09-16 10:46:45 +0530

    updated log messages

--- food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
+++ food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -27,6 +27,8 @@ public class OrderController {
     public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
         log.info("Creating order for customer = {} at restaurant = {}", createOrderCommand.getCustomerId(),
                    createOrderCommand.getRestaurantId());
+        log.debug("Creating order for customer = {} at restaurant = {}", createOrderCommand.getCustomerId(),
+                createOrderCommand.getRestaurantId());
         CreateOrderResponse createOrderResponse = orderApplicationService.createOrder(createOrderCommand);
         log.info("Order created with tracking id -  {} ", createOrderResponse.getOrderTrackingId());
         return ResponseEntity.ok(createOrderResponse);
@@ -37,6 +39,7 @@ public class OrderController {
        TrackOrderResponse trackOrderResponse =
                orderApplicationService.trackOrder(TrackOrderQuery.builder().orderTrackingId(trackingId).build());
        log.info("Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
+       log.debug("Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
        return  ResponseEntity.ok(trackOrderResponse);
     }
 }
