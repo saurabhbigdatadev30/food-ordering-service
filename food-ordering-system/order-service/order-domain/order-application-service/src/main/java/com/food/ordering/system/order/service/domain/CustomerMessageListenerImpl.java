7cca64d1e96d8629856d4dbc5b7437f749c2b82f
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2023-09-19 08:34:29 +0530

    updated

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/CustomerMessageListenerImpl.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/CustomerMessageListenerImpl.java
@@ -25,10 +25,11 @@ public class CustomerMessageListenerImpl implements CustomerMessageListener {
     public void customerCreated(CustomerModel customerModel) {
         Customer customer = customerRepository.save(orderDataMapper.customerModelToCustomer(customerModel));
         if (customer == null) {
-            log.error("Customer could not be created in order database with id: {}", customerModel.getId());
+            log.error("Customer could not be created in order database with id: {}", customerModel.getId() + "TEST");
+            log.info("Customer could not be created in order database with id: {}", customerModel.getId());
             throw new OrderDomainException("Customer could not be created in order database with id " +
                     customerModel.getId());
         }
-        log.info("Customer is created in order database with id: {}", customer.getId());
+       // log.info("Customer is created in order database with id: {}", customer.getId());
     }
 }
