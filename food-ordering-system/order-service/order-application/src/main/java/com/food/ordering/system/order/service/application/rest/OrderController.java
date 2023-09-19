44cdb7bb43e7ace19d293dc84c5dfa97b27c07c2
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2023-09-19 11:12:21 +0530

    modified the log message in OrderController method createOrder()

--- food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
+++ food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -28,7 +28,7 @@ public class OrderController {
         log.info("Creating order for customer = {} at restaurant = {}", createOrderCommand.getCustomerId(),
                    createOrderCommand.getRestaurantId());
         CreateOrderResponse createOrderResponse = orderApplicationService.createOrder(createOrderCommand);
-        log.info("Order created with tracking id -  {} ", createOrderResponse.getOrderTrackingId());
+        log.info("Order created with tracking id ==  {} ", createOrderResponse.getOrderTrackingId());
         return ResponseEntity.ok(createOrderResponse);
     }
 
