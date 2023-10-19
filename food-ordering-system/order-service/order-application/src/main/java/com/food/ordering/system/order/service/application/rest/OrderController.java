0683a367fb013b835fd035602758df61f11a845e
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2023-10-19 19:29:28 +0530

    Revert "updated the test branch"
    This reverts commit 5598777a038f8cefaa8291e0e0ee6d4ce3c39e6b.

--- food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
+++ food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -24,10 +24,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand)
-    {
-        int a = 69;
-        log.info("In TestBranch createOrder method");
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
         log.info("Creating order for customer = {} at restaurant = {}", createOrderCommand.getCustomerId(),
                    createOrderCommand.getRestaurantId());
         CreateOrderResponse createOrderResponse = orderApplicationService.createOrder(createOrderCommand);
